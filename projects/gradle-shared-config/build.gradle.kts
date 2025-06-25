f7f8cd9568d2247ecabfaf3a887f2d2c490e2235
Abhishek <abhishekbhaskar@github.com>
2025-06-25 12:44:38 -0500

    Update build.gradle.kts

--- projects/gradle-shared-config/build.gradle.kts
+++ projects/gradle-shared-config/build.gradle.kts
@@ -10,6 +10,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.0")
 }
 
